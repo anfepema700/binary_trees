ed01f334c130851f5d7336354fe34b34c9614662
totod8 <totod8@hotmail.com>
2022-03-25 15:28:56 -0500

    mycommit

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,25 +1,16 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_leaves - function that counts the leaves in a binary tree
- *
- * @tree: pointer to the root node of the tree to measure the height.
- *
- * Return: If tree is NULL return 0
+ * binary_tree_leaves - counts the leaves in a tree
+ *@tree: pointer to the tree
+ * Return: number of leaves
  */
-
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	size_t left_leave;
-	size_t right_leave;
-
-	if (tree == NULL)
-		return (0);
-
-	if (tree->left == NULL && tree->right == NULL)
-		return (1);
-
-	left_leave = binary_tree_leaves(tree->left);
-	right_leave = binary_tree_leaves(tree->right);
-	return (left_leave + right_leave);
+  if (tree == NULL)
+    return (0);
+  if (tree->left == NULL && tree->right == NULL)
+    return (1);
+  else
+    return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
